07bf137c73dc12532e3bb98b9d8c7a8dde03e2d3
Cheng-Hong <s26803018@gmail.com>
2022-09-26 00:18:48 +0800

    add GRDB

---
+++ File Templates/Custom/GRDB.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,44 @@
+//___FILEHEADER___
+
+import Foundation
+import GRDB
+
+/// The Entity struct.
+///
+/// Identifiable conformance supports SwiftUI list animations, and type-safe
+/// GRDB primary key methods.
+/// Equatable conformance supports tests.
+struct ___FILEBASENAMEASIDENTIFIER___: Identifiable, Equatable {
+    /// The entity id.
+    ///
+    /// Int64 is the recommended type for auto-incremented database ids.
+    /// Use nil for entitys that are not inserted yet in the database.
+    var id: Int64?
+    
+    /// Creates a new entity
+    static func new(id: Int64? = nil) -> Self {
+        .init(id: id)
+    }
+}
+
+// MARK: - Persistence
+
+/// Make entity a Codable Record.
+///
+/// See <https://github.com/groue/GRDB.swift/blob/master/README.md#records>
+extension ___FILEBASENAMEASIDENTIFIER___: Codable, FetchableRecord, PersistableRecord, MutablePersistableRecord {
+    // Define database columns from CodingKeys
+    enum Columns {
+        static let id = Column(CodingKeys.id)
+    }
+    
+    // For a non-codable record
+    // Define database columns as an enum
+//    enum Columns: String, ColumnExpression {
+//        case id
+//    }
+    
+    internal mutating func didInsert(with rowID: Int64, for column: String?) {
+        id = rowID
+    }
+}
---
+++ File Templates/Custom/GRDB.xctemplate/___FILEBASENAME___s.swift
@@ -0,0 +1,115 @@
+//___FILEHEADER___
+
+import GRDB
+import RxGRDB
+import RxSwift
+
+/// ___FILEBASENAMEASIDENTIFIER___ is responsible for high-level operations on the Entitys database.
+struct ___FILEBASENAMEASIDENTIFIER___ {
+    private let dbWriter: DatabaseWriter
+    
+    init(dbWriter: DatabaseWriter) {
+        self.dbWriter = dbWriter
+    }
+    
+    // MARK: - Modify Entitys
+    
+    func insert(_ entity: ___VARIABLE_productName:identifier___) -> Single<Void> {
+        dbWriter.rx.write(updates: { db in try _insert(db, entity: entity) })
+    }
+    
+    func deleteAll() -> Single<Void> {
+        dbWriter.rx.write(updates: _deleteAll)
+    }
+    
+    func deleteOne(_ entity: ___VARIABLE_productName:identifier___) -> Single<Void> {
+        dbWriter.rx.write(updates: { db in try _deleteOne(db, entity: entity) })
+    }
+    
+    // MARK: - Access Entitys
+    
+    func fetchAll() -> Observable<[___VARIABLE_productName:identifier___]> {
+        ValueObservation
+            .tracking(_fetchAll)
+            .rx.observe(in: dbWriter)
+    }
+    
+    /// An observable that tracks changes in the Entitys
+    func EntitysOrderedByID() -> Observable<[___VARIABLE_productName:identifier___]> {
+        ValueObservation
+            .tracking(___VARIABLE_productName:identifier___.all().orderByID().fetchAll)
+            .rx.observe(in: dbWriter)
+    }
+    
+    // MARK: - Implementation
+    //
+    // ⭐️ Good practice: when we want to update the database, we define methods
+    // that accept a Database connection, because they can easily be composed.
+    
+    private func _insert(_ db: Database, entity: ___VARIABLE_productName:identifier___) throws {
+        try entity.insert(db)
+    }
+    
+    private func _deleteAll(_ db: Database) throws {
+        try ___VARIABLE_productName:identifier___.deleteAll(db)
+    }
+    
+    private func _deleteOne(_ db: Database, entity: ___VARIABLE_productName:identifier___) throws {
+        try entity.delete(db)
+    }
+    
+    private func _update(_ db: Database, entity: ___VARIABLE_productName:identifier___) throws {
+        try entity.update(db)
+    }
+    
+    private func _fetchAll(_ db: Database) throws -> [___VARIABLE_productName:identifier___] {
+        try ___VARIABLE_productName:identifier___.fetchAll(db)
+    }
+}
+
+// MARK: - Entity Database Requests
+
+// Define requests of entitys in a constrained extension to the DerivableRequest protocol.
+extension DerivableRequest where RowDecoder == ___VARIABLE_productName:identifier___ {
+    func orderByID() -> Self {
+        return order(RowDecoder.Columns.id.desc)
+    }
+}
+
+// MARK: - For Swift
+
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    
+    // MARK: - Modify Entitys
+    
+    /// 新增entity
+    /// - Parameter entity: 實例
+    func insertEntity(_ entity: ___VARIABLE_productName:identifier___) throws {
+        try dbWriter.write { db in try _insert(db, entity: entity) }
+    }
+    
+    /// 刪除所有entity
+    func deleteAllEntitys() throws {
+        _ = try dbWriter.write(_deleteAll)
+    }
+    
+    /// 刪除單個entity
+    /// - Parameter entity: 實例
+    func deleteOne(_ entity: ___VARIABLE_productName:identifier___) throws {
+        _ = try dbWriter.write { db in try _deleteOne(db, entity: entity) }
+    }
+    
+    /// 更新entity
+    /// - Parameter entity: 實例
+    func updateEntity(_ entity: ___VARIABLE_productName:identifier___) throws {
+        try dbWriter.write { db in try _update(db, entity: entity)}
+    }
+    
+    // MARK: - Access Entitys
+    
+    /// 讀取entity
+    /// - Returns: 所有實例
+    func fetchAllEntitys() throws -> [___VARIABLE_productName:identifier___] {
+        try dbWriter.read(_fetchAll)
+    }
+}
